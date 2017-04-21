08e7caeb23c26681c0d056a651338aa475602210
ileonelperea <leonel@tinkerware.io>
2017-04-21 01:27:26 -0500

    Add icons and stack, database and add-ons sections

--- src/containers/ProvisionerForm.js
+++ src/containers/ProvisionerForm.js
@@ -3,24 +3,30 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { fromJS } from "immutable";
 import { Step, Stepper, StepLabel } from "material-ui/Stepper";
+import { white } from "material-ui/styles/colors";
 import * as applicationActions from "../actions/ApplicationActions";
+import FontIcon from "material-ui/FontIcon";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import Paper from "material-ui/Paper";
 import RaisedButton from "material-ui/RaisedButton";
 import React, { Component, PropTypes } from "react";
 import SwipeableViews from "react-swipeable-views";
 import TextField from "material-ui/TextField";
+import tinkerwareBaseTheme from "../theme/tinkerwareBaseTheme";
 
 export class ServiceForm extends Component {
  render() {
   const style = {
    margin: 12,
   };
+  const customMuiTheme = getMuiTheme(tinkerwareBaseTheme);
   return (
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={customMuiTheme}>
       <div className="small-12 medium-12 large-12 large-centered columns">
+        {console.log(customMuiTheme)}
         <div className="container">
-          <Stepper activeStep={2}>
+          <Stepper activeStep={3}>
             <Step>
               <StepLabel>{"Nombre de proyecto"}</StepLabel>
             </Step>
@@ -31,11 +37,11 @@ export class ServiceForm extends Component {
               <StepLabel>{"DataBase"}</StepLabel>
             </Step>
             <Step>
-              <StepLabel>{"Add-ons"}</StepLabel>
+              <StepLabel>{"Complementos"}</StepLabel>
             </Step>
           </Stepper>
           <SwipeableViews
-              index={1}
+              index={3}
           >
             <div>
               <h1 className="align-center">{"Obtenga entornos de trabajo inmediatos con una configuración rápida y lleve a cualquier nivel"}</h1>
@@ -49,12 +55,192 @@ export class ServiceForm extends Component {
             </div>
             <div>
               <h1 className="align-center">{"Selecciona el Stack que utilizas"}</h1>
-              <TextField
-                  floatingLabelText={"Nombre del Proyecto"}
-                  fullWidth
-                  name={"projectName"}
-                  type="text"
-              />
+              <ul id="hexGrid">
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-react"}/>
+                      <h2>{"React"}</h2>
+                      <p>{"NodeJS"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-erlang"}/>
+                      <h2>{"Erlang"}</h2>
+                      <p>{"Functional programming language"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-angularjs"}/>
+                      <h2>{"Angular"}</h2>
+                      <p>{"NodeJS"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-django"}/>
+                      <h2>{"Django"}</h2>
+                      <p>{"Python"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-rails"}/>
+                      <h2>{"Rails"}</h2>
+                      <p>{"Ruby"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-nodejs"}/>
+                      <h2>{"NodeJS"}</h2>
+                      <p>{"JavaScript environment"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-yii"}/>
+                      <h2>{"Yii"}</h2>
+                      <p>{"PHP"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-ember"}/>
+                      <h2>{"Ember"}</h2>
+                      <p>{"NodeJS"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-meteor"}/>
+                      <h2>{"Meteor"}</h2>
+                      <p>{"Javascript"}</p>
+                    </a>
+                  </div>
+                </li>
+              </ul>
+              <RaisedButton label={"Siguiente"} primary={true} style={style} />
+            </div>
+            <div>
+              <h1 className="align-center">{"Selecciona la Base de Datos que utilizas"}</h1>
+              <ul id="hexGrid">
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <span className="icon icon-cassandra">
+                        <span className="path1"></span>
+                        <span className="path2"></span>
+                        <span className="path3"></span>
+                        <span className="path4"></span>
+                      </span>
+                      <h2>{"Cassandra"}</h2>
+                      <p>{"Base de datos clave-valor"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-redis"}/>
+                      <h2>{"Redis"}</h2>
+                      <p>{"Base de datos en memoria"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-mongodb"}/>
+                      <h2>{"Mongodb"}</h2>
+                      <p>{"Basede datos orientado a documentos"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-mysql"}/>
+                      <h2>{"Mysql"}</h2>
+                      <p>{"Base de datos relacional"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-postgresql"}/>
+                      <h2>{"Postgresql"}</h2>
+                      <p>{"Base de datos relacional"}</p>
+                    </a>
+                  </div>
+                </li>
+              </ul>
+              <RaisedButton label={"Siguiente"} primary={true} style={style} />
+            </div>
+            <div>
+              <h1 className="align-center">{"Selecciona los Complementos que te gustaría tener"}</h1>
+              <ul id="hexGrid">
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <span className="icon icon-cassandra">
+                        <span className="path1"></span>
+                        <span className="path2"></span>
+                        <span className="path3"></span>
+                        <span className="path4"></span>
+                      </span>
+                      <h2>{"Cassandra"}</h2>
+                      <p>{"Base de datos clave-valor"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-redis"}/>
+                      <h2>{"Redis"}</h2>
+                      <p>{"Base de datos en memoria"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-mongodb"}/>
+                      <h2>{"Mongodb"}</h2>
+                      <p>{"Basede datos orientado a documentos"}</p>
+                    </a>
+                  </div>
+                </li>
+                <li className="hex">
+                  <div className="hexIn">
+                    <a className="hexLink" href="#">
+                      <FontIcon className={"icon icon-mysql"}/>
+                      <h2>{"Mysql"}</h2>
+                      <p>{"Base de datos relacional"}</p>
+                    </a>
+                  </div>
+                </li>
+              </ul>
               <RaisedButton label={"Siguiente"} primary={true} style={style} />
             </div>
           </SwipeableViews>
---
+++ src/theme/tinkerwareBaseTheme.js
@@ -0,0 +1,34 @@
+import {
+  cyan500, cyan700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullWhite, fullBlack,
+} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
+
+export default {
+  spacing: spacing,
+  fontFamily: 'SFText-Regular',
+  borderRadius: 2,
+  palette: {
+    primary1Color: "#2fa9b6",
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: pinkA200,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: fullWhite,
+    secondaryTextColor: fade(darkBlack, 0.54),
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+  stepper: {
+    textColor: fullWhite
+  }
+};
