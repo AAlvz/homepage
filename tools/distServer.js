8cac9b7ab980c1de12ac3708d3438394bff1a36e
ileonelperea <leonel@tinkerware.io>
2017-05-04 14:58:39 -0500

    First commit

---
+++ tools/distServer.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/* eslint-disable no-console */
+let app = express();
+let bodyParser = require("body-parser");
+let compression = require("compression");
+let express = require("express");
+let homepageConfiguration = require("../src/homepageConfiguration");
+let jsonParser = bodyParser.json({ type: "application/json" });
+let Mailchimp = require( "mailchimp-v3-api");
+let path = require("path");
+
+app.set("port", 3001);
+
+app.use(compression());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.post("/login/", jsonParser, function (req, res) {
+  let mailchimpAPI = new Mailchimp({
+    key: homepageConfiguration.api_key,
+    debug: false,
+    location: homepageConfiguration.location
+  });
+
+  mailchimpAPI
+    .post("/lists/"+homepageConfiguration.list_id+"/members/", req.body)
+    .then(function(response){
+      res.send(response);
+    });
+});
+
+app.listen(app.get("port"), function() {
+  console.log("Running production build...");
+});
