8528966f51baf3c9f1ee84a6cedc052fbf46d428
ileonelperea <leonel@tinkerware.io>
2017-05-30 17:58:48 -0500

    Update webpack config

--- src/apps/development.js
+++ src/apps/development.js
@@ -12,6 +12,8 @@ import ReactGA from "react-ga";
 import routes from "../routes/development";
 
 require("../tinkerware-share-image.jpg");
+require("../sitemap.xml");
+require("../robots.txt");
 
 ReactGA.initialize("UA-86896105-1");
 
