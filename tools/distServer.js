029b02ea9441e779912645158911e86e2ae3005e
ileonelperea <leonel@tinkerware.io>
2017-06-08 17:24:34 -0500

    Fix express routes

--- tools/distServer.js
+++ tools/distServer.js
@@ -37,6 +37,10 @@ app.post("/login/", jsonParser, function (req, res) {
     });
 });
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 app.listen(app.get("port"), function() {
   console.log("Running production build...");
 });
